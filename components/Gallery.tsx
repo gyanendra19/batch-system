015af5956140d90cf49d1eb8e0311b96a9bd9e61
gyanendra19 <gyanendrav416@gmail.com>
2024-06-02 16:25:44 +0530

    third commit

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -31,7 +31,7 @@ const Gallery = ({setSelectedPage, setLightImage, setOverlay}: ComponentProps) =
             }}
             className='w-full py-10'>
             <h1 className='w-full text-center text-4xl font-bold'>App Gallery</h1>
-            <div className='w-5/6 mx-auto md:grid grid-cols-2 gap-8 mt-9 gap-y-4 px-6'>
+            <div className='md:w-5/6 w-11/12 mx-auto md:grid grid-cols-2 gap-8 mt-9 gap-y-4 px-6'>
                 {galleryPhoto.map(photo => (
                     <Image onClick={() => handleGallery(photo)} className='shadow-2xl mt-4 md:mt-0 cursor-pointer' src={`/${photo}.png`} alt='gallery-1' width={500} height={500} />
                 ))}
